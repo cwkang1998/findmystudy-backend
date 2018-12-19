6f2152041cc05e4ae064721abfd95fe0e4cb70bf
chenwenkang <cwk1998@hotmail.com>
2018-12-19 17:25:30 +0800

    added options to use new parser to remove deprecated warning

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var config = require("./config");
 
-mongoose.connect(config.DB_URL);
+mongoose.connect(
+    config.DB_URL,
+    { useNewUrlParser: true }
+);
 mongoose.Promise = global.Promise;
-mongoose.connection.on("error", console.error.bind(console, "mongodb connection err:"));
+mongoose.connection.on(
+    "error",
+    console.error.bind(console, "mongodb connection err:")
+);
+
 var app = express();
 
 app.use(logger("tiny"));
@@ -15,16 +22,16 @@ app.use(bodyParser.json());
 //Handles no matching url
 app.use((req, res, next) => {
     var err = {
-        description:"404 not found",
-        status:404
-    }
+        description: "404 not found",
+        status: 404
+    };
     next(err);
 });
 
 //Handles error for server
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
-    res.json({ error: err.description});
+    res.json({ error: err.description });
 });
 
 module.exports = app;
