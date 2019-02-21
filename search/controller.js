c8a58a296fb82642b585a5c859eeb31fe892348f
chenwenkang <cwk1998@hotmail.com>
2019-02-21 17:56:35 +0800

    completed general query functionality for controllers in search module

--- search/controller.js
+++ search/controller.js
@@ -3,19 +3,59 @@ const University = models.University;
 const Course = models.Course;
 
 function ListUnisController(req, res, next) {
-    res.json({"uni": "uni"});
+    University.find({}, "_id name description address country", (err, data) => {
+        if (err) {
+            res.status(400).json({err});
+            return;
+        }
+        res.status(200).json(data);
+    });
 }
 
 function RetreiveUniController(req, res, next) {
-    res.json({"uni": "uni"});
+    University.findById(
+        {_id: req.params.id},
+        "_id name description address country",
+        (err, data) => {
+            if (err) {
+                res.status(400).json({err});
+                return;
+            }
+            if (data === null) {
+                res.status(404).json({err: "Document with that id does not exist."});
+                return;
+            }
+            res.status(200).json(data);
+        }
+    );
 }
 
 function ListCourseController(req, res, next) {
-    res.json({"uni": "uni"});
+    Course.find({}, "_id name university description cost", (err, data) => {
+        if (err) {
+            res.status(400).json({err});
+            return;
+        }
+        res.status(200).json(data);
+    });
 }
 
 function RetrieveCourseController(req, res, next) {
-    res.json({"uni": "uni"});
+    Course.findById(
+        {_id: req.params.id},
+        "_id name university description cost",
+        (err, data) => {
+            if (err) {
+                res.status(400).json({err});
+                return;
+            }
+            if (data === null) {
+                res.status(404).json({err: "Document with that id does not exist."});
+                return;
+            }
+            res.status(200).json(data);
+        }
+    );
 }
 
 module.exports = {
