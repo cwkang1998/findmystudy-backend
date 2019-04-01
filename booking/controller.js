6b579246b8012c3108c1f1aaf5252cb8ba57e3f7
chenwenkang <cwk1998@hotmail.com>
2019-04-02 02:05:25 +0800

    fix issue with email and phone number validation for booking creation endpoint

--- booking/controller.js
+++ booking/controller.js
@@ -9,49 +9,59 @@ const Booking = models.Booking;
  * @param {Function} next
  */
 async function ListBookingController(req, res, next) {
-  let query = Booking.find(
-    {},
-    "_id student phone_number email booking_time status"
-  ).sort("-created_time");
-  try {
-    let data = await query.exec();
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(400).json({ err: err.message });
-    return;
-  }
+    let query = Booking.find(
+        {},
+        "_id student phone_number email booking_time status"
+    ).sort("-created_time");
+    try {
+        let data = await query.exec();
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(400).json({ err: err.message });
+        return;
+    }
 }
 
 async function CreateBookingController(req, res, next) {
-  const body = req.body;
-  try {
-    let data = await Booking.create({
-      student: ObjectId(body.student),
-      phone_number: body.phone_number ? body.phone_number : "",
-      email: body.email,
-      booking_time: body.booking_time
-    });
-    res.status(201).json({
-      _id: data._id,
-      student: data.student,
-      phone_number: data.phone_number,
-      email: data.email,
-      booking_time: data.booking_time
-    });
-  } catch (err) {
-    message = err.message;
-    if (
-      err.message ==
-      "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
-    ) {
-      message = `ObjectID Validation Error: ${err.message}`;
+    const body = req.body;
+    let bookingData = {
+        student: ObjectId(body.student),
+        booking_time: body.booking_time,
+        phone_number: ""
+    };
+    if (body.phone_number) {
+        if (body.phone_number.slice(0, 2) != "+6") {
+            bookingData.phone_number = `+6${body.phone_number}`;
+        }else{
+          bookingData.phone_number = body.phone_number;
+        }
     }
-    res.status(400).json({ err: message });
-    return;
-  }
+    if (body.email) {
+        bookingData.email = body.email;
+    }
+    try {
+        let data = await Booking.create(bookingData);
+        res.status(201).json({
+            _id: data._id,
+            student: data.student,
+            phone_number: data.phone_number,
+            email: data.email,
+            booking_time: data.booking_time
+        });
+    } catch (err) {
+        message = err.message;
+        if (
+            err.message ==
+            "Argument passed in must be a single String of 12 bytes or a string of 24 hex characters"
+        ) {
+            message = `ObjectID Validation Error: ${err.message}`;
+        }
+        res.status(400).json({ err: message });
+        return;
+    }
 }
 
 module.exports = {
-  ListBookingController,
-  CreateBookingController
+    ListBookingController,
+    CreateBookingController
 };
